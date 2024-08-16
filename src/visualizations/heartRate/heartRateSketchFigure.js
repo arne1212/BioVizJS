9ebf8c2a676ede79c8f3a36c5cf673f3be39bc83
Arne Stramka <106690069+arne1212@users.noreply.github.com>
2024-08-16 19:33:04 +0200

    reworked sketchFigure animation to also be supported in safari browser

--- src/visualizations/heartRate/heartRateSketchFigure.js
+++ src/visualizations/heartRate/heartRateSketchFigure.js
@@ -20,6 +20,8 @@ export class HeartRateSketchFigure extends HeartRateVisualization {
         // class constants for the y values of the highest and lowest level
         this.minY = 89;
         this.maxY = 176;
+        this.currentY = this.maxY; // Neue Eigenschaft
+        this.phase = 0; // Neue Eigenschaft
 
         this.validateAndSetOptions(options);
         this.draw();
@@ -77,63 +79,103 @@ export class HeartRateSketchFigure extends HeartRateVisualization {
                 <path d="M132.244 148H133.6C135.809 148 137.6 149.791 137.6 152V178.964C137.6 182.067 140.98 183.988 143.646 182.401L143.954 182.218C145.215 181.467 146.785 181.467 148.046 182.218L148.354 182.401C151.02 183.988 154.4 182.067 154.4 178.964V152C154.4 149.791 156.191 148 158.4 148H159.756C162.058 148 163.885 146.062 163.749 143.764L161.973 113.764C161.848 111.65 160.097 110 157.98 110H134.02C131.903 110 130.152 111.65 130.027 113.764L128.251 143.764C128.115 146.062 129.942 148 132.244 148Z"/>
             </clipPath>
             </defs>
-
+    
         <g id="valueDisplay" visibility="${this.valueVisible ? 'visible' : 'hidden'}">
             <text id="currentVal" x="146" y="80" font-size="0.6em" text-anchor="middle" dominant-baseline="middle" fill="${svgBaseColor}" stroke="none">0</text>
         </g>
         
         <g id="filling" clip-path="url(#bodyClip)">
-            
-            ${this.isAnimated ?
-                `<path id="wave" fill="none">
-                <animate id="waveAnimation" attributeName="d" values="
-                M128,${y} C134,${y-2} 140,${y+2} 146,${y} S158,${y-2} 164,${y} L164,616 L128,616 Z;
-                M128,${y-2} C134,${y} 140,${y-4} 146,${y-2} S158,${y} 164,${y-2} L164,616 L128,616 Z;
-                M128,${y} C134,${y-2} 140,${y+2} 146,${y} S158,${y-2} 164,${y} L164,616 L128,616 Z"
-                dur="3s" repeatCount="indefinite" fill="none"/>
-                <animate attributeName="y" values="184;110;184" dur="5s" repeatCount="indefinite" />
-                </path>`
-                :
-                `<rect id="fillBox" x="128" y="${y}" width="100%" height="100%" fill="none"></rect>`}                          
+            ${this.isAnimated 
+                ? `<path id="wave" fill="transparent" d="M128,${y} C134,${y} 140,${y} 146,${y} S158,${y} 164,${y} L164,616 L128,616 Z"></path>`
+                : `<rect id="fillBox" x="128" y="${y}" width="36" height="440" fill="transparent"></rect>`
+            }                          
         </g>
-
+    
         <g id="stick_figure" stroke="black" stroke-width="1"> 
             <circle id="head" cx="146" cy="98" r="9.5"/>
             <path id="body" d="M132.244 148H133.6C135.809 148 137.6 149.791 137.6 152V178.964C137.6 182.067 140.98 183.988 143.646 182.401L143.954 182.218C145.215 181.467 146.785 181.467 148.046 182.218L148.354 182.401C151.02 183.988 154.4 182.067 154.4 178.964V152C154.4 149.791 156.191 148 158.4 148H159.756C162.058 148 163.885 146.062 163.749 143.764L161.973 113.764C161.848 111.65 160.097 110 157.98 110H134.02C131.903 110 130.152 111.65 130.027 113.764L128.251 143.764C128.115 146.062 129.942 148 132.244 148Z"/>
         </g>
         </svg>
         `;
-        // get safe reference to the svg element of the instance to avoid naming conflicts with other DOM elements
         const tempDiv = document.createElement('div');
         tempDiv.innerHTML = svgCode;
-        const newSvg = tempDiv.firstElementChild;
-        this.svgElement = newSvg;
-        this.container.appendChild(newSvg);
+        this.svgElement = tempDiv.firstElementChild;
+        this.container.appendChild(this.svgElement);
+        
+        // start animation
+        if (this.isAnimated) {
+            requestAnimationFrame(this.animate);
+        }       
     }
 
     update(heartRate) {
-        if (!this.svgElement) {
-            return;
-        }
+        if (!this.svgElement) return;
+    
         const fillLevelY = this.calculateFillLevel(heartRate);
         const fillColor = this.calculateFillColor(heartRate);
-        var animatedPath = `M128,${fillLevelY} C134,${fillLevelY-2} 140,${fillLevelY+2} 146,${fillLevelY} S158,${fillLevelY-2} 164,${fillLevelY} L164,616 L128,616 Z;
-                M128,${fillLevelY-2} C134,${fillLevelY} 140,${fillLevelY-4} 146,${fillLevelY-2} S158,${fillLevelY} 164,${fillLevelY-2} L164,616 L128,616 Z;
-                M128,${fillLevelY} C134,${fillLevelY-2} 140,${fillLevelY+2} 146,${fillLevelY} S158,${fillLevelY-2} 164,${fillLevelY} L164,616 L128,616 Z`;
+        this.currentY = fillLevelY;
+
+    
         if (this.isAnimated) {
-            var waveElement = this.svgElement.querySelector("#wave");
-            waveElement.style.fill = fillColor;
-            var animatedWave = wave.querySelector("#waveAnimation");
-            animatedWave.setAttribute("values", animatedPath);
+            const wave = this.svgElement.querySelector("#wave");
+            if (wave) {
+                wave.style.fill = fillColor;
+            }
+        } else {
+            const fillRect = this.svgElement.querySelector("#fillBox");
+            if (fillRect) {
+                fillRect.setAttribute("y", fillLevelY);
+                fillRect.setAttribute("fill", fillColor);
+                fillRect.setAttribute("height", `${this.maxY - fillLevelY + 440}`);
+            }
         }
-        else {
-            var fillRect = this.svgElement.querySelector("#fillBox");
-            fillRect.setAttribute("y", fillLevelY);
-            fillRect.setAttribute("fill", fillColor);
-        }
-        // update the displayed value
+    
+        // Update the displayed value
         const currentValue = this.svgElement.querySelector('#currentVal');
-        currentValue.textContent = heartRate;
+        if (currentValue) {
+            currentValue.textContent = heartRate;
+        }
+    }
+
+    animate = () => {
+        this.updatePathWave();
+        requestAnimationFrame(this.animate);
+    }
+
+    updatePathWave() {
+        const path = this.svgElement.querySelector('#wave');
+        const width = 36;
+        const baseY = this.currentY;
+        const amplitude = 1.5;
+        const frequency = 1;
+    
+        const calculateY = (x) => {
+            // phase value determines vertical movements of wave
+        return baseY + amplitude * Math.sin(frequency * x + this.phase);
+        };
+    
+        // calculate points of the wave
+        const points = [];
+        const numPoints = 10; // number of points to determine smootheness of the animation
+        for (let i = 0; i <= width; i += width / (numPoints - 1)) {
+        points.push(`${128 + i},${calculateY(i)}`);
+        }
+    
+        // create the path
+        const d = `M${points[0]} ` +
+        // select all points except the first and last and concatenate as string
+        `C${points.slice(1, -1).join(' ')} ` +
+        `${points[points.length - 1]} ` +
+        `L164,616 L128,616 Z`;
+    
+        path.setAttribute('d', d);
+        
+        // by increasing the phase value the vertical position of the points changes each frame
+        // this creates the illusion of a wave moving from right to left
+        this.phase += 0.05;
+        if (this.phase > 2 * Math.PI) {
+            this.phase -= 2 * Math.PI; // phase in [0, 2π] to avoid numeric instability for increasing phase values
+        }
     }
 
     /**
