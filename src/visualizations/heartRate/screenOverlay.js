33ea618b50400ba379c625238bf756972dae86e4
Arne Stramka <106690069+arne1212@users.noreply.github.com>
2024-08-17 11:55:42 +0200

    set reasonable default values for screenOverlay

--- src/visualizations/heartRate/screenOverlay.js
+++ src/visualizations/heartRate/screenOverlay.js
@@ -29,10 +29,6 @@ export class ScreenOverlay extends HeartRateVisualization {
         this.maxVal;
         this.tunnelIntensity;
 
-        // allows the vignette/tunnel effect to be scroll independent
-        // this.container.style.position = 'fixed';
-        // this.container.style.top = 0;
-        // this.container.style.left = 0;
         // how much the shadow fades as opposed to sharp edges
         this.shadowBlur;
 
@@ -58,27 +54,35 @@ export class ScreenOverlay extends HeartRateVisualization {
         if ('color' in options) {
             if(!isValidColor(options.color)) {
                 this.color = "rgba(0,0,0,";
-                throw new Error(noValidColorErrorMessage + ". Default color is black.");
+                console.error(noValidColorErrorMessage + ". Default color is black.");
             }
             else {
                 this.color = this.transformColor(options.color);
             }
         }
+        else {
+            this.color = "red"
+        }
 
         if ('maxValue' in options) {
             if (options.maxValue <= this.referenceVal) {
                 this.maxVal = Math.max(this.referenceVal + 20, 140);
-                throw new Error(`maxValue '${options.maxValue}' must not be smaller than referenceValue '${this.referenceVal}' and is therefore set to '${this.maxVal}'.`);
+                console.error(`maxValue '${options.maxValue}' must not be smaller than referenceValue '${this.referenceVal}' and is therefore set to '${this.maxVal}'.`);
             }
             else {
                 this.maxVal = options.maxValue;
             }
         }
+        else {
+            this.maxVal = Math.max(this.referenceVal + 20, 140);
+            console.info(`maxValue must be set to determine the extent of the tunnel effect and is set to '${this.maxVal}' by default.`)
+        }
+
         const tunnelIntensityDefault = 0.1;
         if ('tunnelIntensity' in options) {
             if(typeof options.tunnelIntensity !== "number" || options.tunnelIntensity < 0 || options.tunnelIntensity > 1) {
                 this.tunnelIntensity = tunnelIntensityDefault;
-                throw new Error(`tunnelIntensity '${options.tunnelIntensity}' must be a input between 0 and 1. It is now set to the default value 0.1`);
+                console.error(`tunnelIntensity '${options.tunnelIntensity}' must be a input between 0 and 1. It is now set to the default value 0.1`);
             }
             else {
                 // user has the posibility to declare intuitiv values between 0 and 1
@@ -123,17 +127,13 @@ export class ScreenOverlay extends HeartRateVisualization {
     }
 
     update(heartRate) {
-
-        console.log('is updated')
         const containerWidth = this.getContainerWidth();
         const containerDiv = this.container;
         const shadowSpread = this.calculateShadowSpread(heartRate, containerWidth);
         const shadowBlur = this.calculateShadowBlur(containerWidth);
         const shadowOpacity = this.calculateShadowOpacity(heartRate);
         const def = `0 0 ${shadowBlur}em ${shadowSpread}px ${this.color + shadowOpacity}) inset`
-        console.log(def)
         containerDiv.style.boxShadow = def;
-        console.log(containerDiv.style.boxShadow)
     }
 
     getContainerWidth() {
@@ -144,7 +144,6 @@ export class ScreenOverlay extends HeartRateVisualization {
 
     calculateShadowBlur(containerWidth) {
         const shadowBlur = Math.pow(containerWidth, 0.8) * 0.02;
-        console.log(shadowBlur)
         return shadowBlur;        
     }
 
